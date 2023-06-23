141c3f9a2c992b22d0e9c39b49e7049146f96284
idouidi <baghdadi.douidi@hotmail.fr>
2023-06-23 16:00:09 +0200

    apolo server v4 can have the schema in param

--- srcs/back-end/ft_transcendence/src/app.module.ts
+++ srcs/back-end/ft_transcendence/src/app.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
-import { ApolloDriver } from '@nestjs/apollo';
 import { UsersModule } from './users/users.module';
 import { ChanelModule } from './chanel/chanel.module';
 import { MessagesModule } from './messages/messages.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ContactsModule } from './contacts/contacts.module';
+import { join } from 'path';
+
+
 
 @Module({
 	imports: [
-		GraphQLModule.forRoot({
-			autoSchemaFile: join(process.cwd(), 'src/schemas.gql'),
-			driver: ApolloDriver,
-			playground: true,
-		}),
+		GraphQLModule.forRoot<ApolloDriverConfig>({
+            autoSchemaFile: join(process.cwd(), 'src/schemas.gql'),
+            driver: ApolloDriver,
+            playground: true,
+        }),
 		UsersModule,
 		ChanelModule,
 		MessagesModule,
